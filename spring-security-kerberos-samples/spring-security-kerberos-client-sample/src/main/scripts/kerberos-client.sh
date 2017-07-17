612b9ecec5ce70020967acff6957644904ce86d4
Andy Gherna <argherna@gmail.com>
2017-07-17 16:20:33 -0500

    Generate consistent deployment modules
    This addresses #4 for the kerberos samples.

---
+++ spring-security-kerberos-samples/spring-security-kerberos-client-sample/src/main/scripts/kerberos-client.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+#
+# kerberos-client.sh
+#
+# Runs the kerberos-client-sample. This is not a server process.
+#
+# author: Andy Gherna (agherna@illinois.edu)
+#
+# ------------------------------------------------------------------------------
+
+
+# Find the directory we're running in.
+#
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$(dirname "$SOURCE")"
+while [ -h "$SOURCE" ]
+do
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+DIR="$(dirname "$DIR")"
+
+# Set the name of the jar file
+#
+JAR_FILE=$DIR/spring-security-kerberos-client-sample-1.0.0-SNAPSHOT.jar
+
+/usr/bin/java -jar $JAR_FILE
